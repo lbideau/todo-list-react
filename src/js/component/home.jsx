cc00242d1088047430d26a69f665cf12194b8914
lbideau <leshliebzt@gmail.com>
2021-09-13 13:32:12 -0400

    New Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,58 @@
 import React from "react";
+import { useState } from "react";
+import { homeStyle } from "./style";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState([]);
+	let nuevoValorTareas = tareas.map(function(item, index) {
+		return (
+			<li key={index}>
+				{item}
+				<i
+					className="fa fa-trash"
+					onClick={event => deleteTodo(index)}
+					aria-hidden="true"></i>
+			</li>
+		);
+	});
+
+	const deleteTodo = position => {
+		var newList = tareas.filter((tarea, index) => index !== position);
+		setTareas(newList);
+		
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<React.Fragment>
+			<div className="container">
+				<span>todos</span>
+				<div className="row">
+					<input
+						type="text"
+						placeholder="Whats needs to be done?"
+						onKeyPress={event =>
+							event.key === "Enter" &&
+							setTareas([...tareas, event.target.value])
+						}
+					/>
+				</div>
+				<div className="row">
+					<ul>{nuevoValorTareas}</ul>
+					<div className="row total">
+						{tareas.length} Total de Tareas
+					</div>
+				</div>
+			</div>
+			<div
+				className="d-flex justify-content-center"
+				style={homeStyle.footer}>
+				Nota: Puedes eliminar una tarea haciendo clic sobre ella
+			</div>
+		</React.Fragment>
 	);
 };
 
